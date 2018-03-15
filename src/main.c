8a2067aba17535ed3f8b5f9038cc1e42501a99ac
herulume <herulume@protonmail.com>
2018-03-15 15:19:40 +0000

    Change code base

--- src/main.c
+++ src/main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include "interface.h"
-#include "parser.h"
 
 int main(int argc, char *argv[]){
   Date d = createDate(1,2,2017);
   printf("Dia: %i\n", get_day(d));
 
-  int i = parse(argv[1]);
+//  int i = parse(argv[1]);
 
-  printf("%d", i);
+//  printf("%d", i);
   return 0;
 }
