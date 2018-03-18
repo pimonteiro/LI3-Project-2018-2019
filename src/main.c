b06b106edf9c5cbbca1a661f4403345447ac69ca
herulume <herulume@protonmail.com>
2018-03-18 21:31:57 +0000

    Add function to check which file is being parsed

--- src/main.c
+++ src/main.c
@@ -1,12 +1,42 @@
 #include <stdio.h>
 #include "interface.h"
+#include <string.h>
+#include "parser.h"
 
+
+#define RED "\033[31m"
+#define BLUE "\033[34m"
+#define GREEN "\033[32m"
+
+#define RESET "\033[0m"
 int main(int argc, char *argv[]){
   Date d = createDate(1,2,2017);
   printf("Dia: %i\n", get_day(d));
 
-//  int i = parse(argv[1]);
 
-//  printf("%d", i);
-  return 0;
+ int i;
+ for(i=1; i<argc;i++){
+  printf("Ficheiro: %s%s%s ", BLUE, argv[i], RESET);
+  if(isFile(argv[i]) == 2)
+    printf("%sPosts%s\n", GREEN, RESET);
+  else if(isFile(argv[i]) == 1)
+    printf("%sUsers%s\n", GREEN, RESET);
+  else printf("%sNão importa%s\n", RED, RESET);
+ }
+}
+
+int isFile(const char *str)
+{
+    if (!str)
+        return 0;
+    size_t lenStr = strlen(str);
+    // -4 = .xml
+    // -2 = usErs.xml or poSts.xml
+    // 6
+    if(*(str + lenStr - 7) == 'e')
+        return 1;
+    else if(*(str + lenStr - 7) == 's')
+        return 2;
+    else
+        return -1;
 }
--- src/parser.c
+++ src/parser.c
@@ -5,19 +5,39 @@
 #include <glib.h>
 
 
-/**Função que é chamada quando um elemento é encontrado
-r* @ void* user_data: apontador para a nossa estrutura de dados
- * @ const xmlChar* fullname: nome do elemento encontrado
- * @ const xmlChar** attrs: atributos dos elementos encontrados (o nossa informação)
- *
- */
-void xmlSAX2StartElement(void* user_data, const xmlChar *fullname, const xmlChar **attrs) {
+/*
+
+  XML SAX Insanity.
+
+  Message to professors, classmates and the compiler:
+
+
+  If you want to understand this, you have to hit a blunt.
+  I don't care if you're at work. Roll a fat one and smoke it. Right now.
+
+  If your boss questions this, feel free to tell him to contact me. I'll explain how crucial THC is for this shit.
+
+
+  Once you're done trying to 'optimize' this routine, and have realized what a terrible mistake that was,
+  please increment the following counter as a warning for the next guy:
+
+  total_hours_wated_here = 42
+*/
+
+static void error(void *user_data, const char *msg, ...){
+  va_list args;
+
+  va_start(args, msg);
+  printf("\n%s\n", msg);
+  va_end(args);
+}
+
+ void startElement(void* user_data, const xmlChar *fullname, const xmlChar **attrs) {
   GPtrArray* garray =  (GPtrArray*)user_data; // cast para o tipo da nossa estrutura
 
   if(!xmlStrcasecmp(fullname, (const xmlChar*)"posts"))
    printf("\n\n%s\n\n", fullname); // testar se estamos no posts
 
-  // verifica se existe atributo
   while (attrs && *attrs) {
     xmlChar* s = xmlStrdup(attrs[1]); // passar de const xmlChar* para xmlChar*
     g_ptr_array_add(garray, s); // por na nossa estrutura
@@ -26,40 +46,24 @@ void xmlSAX2StartElement(void* user_data, const xmlChar *fullname, const xmlChar
  }
 }
 
-void my_error(void* user_data, const char* msg,...){
-  va_list args;
 
-  va_start(args, msg);
-  g_logv("XML", G_LOG_LEVEL_CRITICAL, msg, args);
-  va_end(args);
 
-}
-/**Função que faz parse aos ficheiros :D
- * Recebe o caminho do xml
- */
 int parse(const char* xml_path) {
-  /** A nossa estrutura:
-   *  Array de apontadores do Glib
-   *  Assumo que tenho 500 pointers pelo menos
-   *    fica mais rapido
-   */
   GPtrArray * garray = g_ptr_array_sized_new(1000);
   g_ptr_array_set_free_func(garray, xmlFree); // função para limpar os elementos do array
 
-  // Estrutura que guarda o estado do XML e trata de o processar
   xmlSAXHandler handler = {0};
-  // Atribuir a função que é chamada quando um elemento é encontrado
-  handler.startElement = xmlSAX2StartElement;
-  handler.error = my_error;
+
+  handler.initialized = XML_SAX2_MAGIC;
+  handler.startElement = startElement;
+
+  handler.warning = error;
+  handler.error = error;
+  handler.fatalError = error;
 
 
   int ctxt;
-  // Carregar o ficheiro
   ctxt = xmlSAXUserParseFile(&handler, garray, xml_path);
-
-//char* s = g_ptr_array_index(garray, 99);
-//printf("retrieving %s\n", s);
-
 
 xmlCleanupParser(); // Limpar o parser
 g_ptr_array_free (garray, TRUE); // Limpar o array
