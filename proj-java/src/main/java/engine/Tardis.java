8de4ad401ab828fbd657fb61123f08fbd36148fb
herulume <herulume@protonmail.com>
2018-06-09 23:51:46 +0100

    Add basic first draft of Tardis

--- proj-java/src/main/java/engine/Tardis.java
+++ proj-java/src/main/java/engine/Tardis.java
@@ -1,8 +1,35 @@
 package engine;
 
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeSet;
+
 public class Tardis {
 
-    private int years;
+    // TODO PROPER DO THIS
 
-    private static final int dias_meses = 31 * 12;
+    private List<List<TreeSet<Question>>> questions;
+    private List<List<TreeSet<Answer>>> answers;
+
+    public Tardis(){
+        this.answers = new ArrayList<>(new ArrayList<>(new TreeSet<>()));
+        this.questions  = new ArrayList<>(new ArrayList<>(new TreeSet<>()));
+
+    }
+
+    public Question getQuestion(LocalDateTime d){
+        int day_index = d.getYear() - 2008;
+        int monthDay_index = d.getMonthValue() -1 + (d.getDayOfYear() -1);
+
+        return this.questions.get(day_index).get(monthDay_index).first();
+    }
+
+    public Answer getAnswer(LocalDateTime d){
+        int day_index = d.getYear() - 2008;
+        int monthDay_index = d.getMonthValue() -1 + (d.getDayOfYear() -1);
+
+        return this.answers.get(day_index).get(monthDay_index).first();
+    }
 }
