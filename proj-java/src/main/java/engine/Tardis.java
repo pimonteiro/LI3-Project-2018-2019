b1f5e2da3a069390774552bc30980bf90fd82ef1
herulume <herulume@protonmail.com>
2018-06-10 14:29:56 +0100

    Change tardis to use hashmaps and add insert method

--- proj-java/src/main/java/engine/Tardis.java
+++ proj-java/src/main/java/engine/Tardis.java
@@ -1,35 +1,69 @@
 package engine;
 
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.TreeSet;
+import java.util.Map;
 
 public class Tardis {
 
-    // TODO PROPER DO THIS
+    // TODO First draft
 
-    private List<List<TreeSet<Question>>> questions;
-    private List<List<TreeSet<Answer>>> answers;
+    private Map<Long,Map<Long,List<Question>>> questions;
+    private Map<Long,Map<Long,List<Answer>>> answer;
 
     public Tardis(){
-        this.answers = new ArrayList<>(new ArrayList<>(new TreeSet<>()));
-        this.questions  = new ArrayList<>(new ArrayList<>(new TreeSet<>()));
+        this.questions = new HashMap<>();
+        this.answer = new HashMap<>();
+    }
+
+    public void insert(Object o){
+        if(!(o instanceof Post)) return;
+        Post tmp = (Post) o;
+
+        long index_year = tmp.getCreation_date().getYear() - 2008;
+        long index_mes = (tmp.getCreation_date().getDayOfMonth() - 1) + (31 * (tmp.getCreation_date().getMonthValue()
+                                                                               - 1));
+
+        if(tmp instanceof Question){
+
+            Map<Long,List<Question>> anos = this.questions.get(index_year);
+            if(anos == null){
+                this.questions.put(index_year, new HashMap<>());
+                anos = this.questions.get(index_year);
+            }
+
+            List<Question> quests = anos.get(index_mes);
+            if(quests == null){
+                anos.put(index_mes, new ArrayList<>());
+                quests = anos.get(index_mes);
+            }
+
+            Question q = (Question) tmp;
+            quests.add(q);
+        }
+
+        if(tmp instanceof Answer){
+
+            Map<Long,List<Answer>> anos = this.answer.get(index_year);
+            if(anos == null){
+                this.answer.put(index_year, new HashMap<>());
+                anos = this.answer.get(index_year);
+            }
+
+            List<Answer> ans = anos.get(index_mes);
+            if(ans == null){
+                anos.put(index_mes, new ArrayList<>());
+                ans = anos.get(index_mes);
+            }
+
+            Answer a = (Answer) tmp;
+            ans.add(a);
+        }
+
 
     }
 
-    public Question getQuestion(LocalDateTime d){
-        int day_index = d.getYear() - 2008;
-        int monthDay_index = d.getMonthValue() -1 + (d.getDayOfYear() -1);
 
-        return this.questions.get(day_index).get(monthDay_index).first();
-    }
-
-    public Answer getAnswer(LocalDateTime d){
-        int day_index = d.getYear() - 2008;
-        int monthDay_index = d.getMonthValue() -1 + (d.getDayOfYear() -1);
-
-        return this.answers.get(day_index).get(monthDay_index).first();
-    }
 }
