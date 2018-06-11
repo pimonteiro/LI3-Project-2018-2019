78fa201b47032285b7cd282f9acac46cd62b259a
herulume <herulume@protonmail.com>
2018-06-11 16:08:53 +0100

    Add first implementation of query11

--- proj-java/src/main/java/engine/Query11.java
+++ proj-java/src/main/java/engine/Query11.java
@@ -1,5 +1,7 @@
 package engine;
 
+import common.Exceptions.NoTagFoundException;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.*;
@@ -14,22 +16,33 @@ public class Query11 {
         topProfiles.addAll(cum.getProfiles().values());
 
 
-        Map<String,Long> tags = topProfiles.stream()
-                                           .limit(N)
-                                           .map(Profile::getPosts)
-                                           .flatMap(set -> set.stream()
-                                                              .filter(p -> p.isBetweenDates(begin.atStartOfDay(),
-                                                                                            end.atTime(LocalTime.MAX))
-                                                                           && (p instanceof Question))
-                                                              .map(Question.class::cast).map(Question::getTags)
-                                                              .map(f -> (f.split(
-                                                                      "<(.*?)><(.*?)><(.*?)><(.*?)><(.*?)>")))
-                                                              .flatMap(Arrays::stream)
+        return topProfiles.stream()
+                          .limit(N)
+                          .map(Profile::getPosts)
+                          .flatMap(set -> set.stream()
+                                             .filter(p -> p.isBetweenDates(begin.atStartOfDay(),
+                                                                           end.atTime(LocalTime.MAX))
+                                                          && (p instanceof Question))
+                                             .map(Question.class::cast).map(Question::getTags)
+                                             .map(f -> (f.split(
+                                                     "<(.*?)><(.*?)><(.*?)><(.*?)><(.*?)>")))
+                                             .flatMap(Arrays::stream)
 
-                                           )
-                                           .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+                          )
+                          .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                          .entrySet()
+                          .stream()
+                          .map(f -> new AbstractMap.SimpleEntry<>(f.))
+                          .sorted(Comparator.comparing(Map.Entry::getValue, Comparator.reverseOrder()))
+                          .limit(N)
+                          .map(Map.Entry::getKey)
+                          .map(f -> {
+                              try{
+                                  return cum.getTag((String) f);
+                              }catch(NoTagFoundException e){
+                                  return null;
+                              }
+                          }).filter(Objects::nonNull).collect(Collectors.toList());
 
-
-        return null;
     }
 }
